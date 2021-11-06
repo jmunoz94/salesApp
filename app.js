2172f1ca4818fdf175230ad4034106f59f96c39d
teresaAgreda <teresaagreda2@gmail.com>
2021-11-06 12:36:59 -0500

    creating route files and adding to app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import router from './routes.js';
 import cors from 'cors';
 
 const app = express();
@@ -6,5 +7,13 @@ const port = 4000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/', router);
 
 app.listen(port, () => {});
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    .json({
+        status: 'error',
+        message: err.message
+    });
+});
